d0bd92930cb1822e2c8f82c0da3d36570d139b62
Gengchen Tuo <tuogengchen@gmail.com>
2021-11-18 21:13:31 -0700

    Updates

---
+++ precache-manifest.dd08730d55f342bb32683be164f9a2d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a272fbb6f45645e033929f1a3dee6ee9",
+    "url": "/cf-plot/index.html"
+  },
+  {
+    "revision": "62400cb7a6a4da4bed2a",
+    "url": "/cf-plot/static/css/main.6cbf1111.chunk.css"
+  },
+  {
+    "revision": "43b58da955f49ff8281f",
+    "url": "/cf-plot/static/js/2.72c89b36.chunk.js"
+  },
+  {
+    "revision": "391cc4d6bc99c625ef6ab439d68645d6",
+    "url": "/cf-plot/static/js/2.72c89b36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62400cb7a6a4da4bed2a",
+    "url": "/cf-plot/static/js/main.69b7d2af.chunk.js"
+  },
+  {
+    "revision": "711754f0f313ce177239",
+    "url": "/cf-plot/static/js/runtime-main.537ec42e.js"
+  }
+]);
